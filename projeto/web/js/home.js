9bf5a31fe428adce941a0510c2f5935b591caa0e
dnovaes <novaesdigo@gmail.com>
2017-08-22 22:42:50 -0300

    atualizado acess_token do graph api explore do facebook e adicionado função de scroll ao buscar o cep

--- projeto/web/js/home.js
+++ projeto/web/js/home.js
@@ -1,3 +1,11 @@
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
         var endereco_completo = conteudo.logradouro + ", " + conteudo.bairro + ", " + conteudo.localidade + " - " + conteudo.uf;
@@ -28,11 +36,16 @@ function callbackLikesFB(content){
   var likesfb = document.getElementById("likesFb");
   likesfb.value = content.fan_count;
   likesfb.innerHTML = content.fan_count;
+  console.log(content.fan_count);
   likesfb.style.display="none";
 }
 
+
 document.addEventListener("DOMContentLoaded", function(){
   var script = document.createElement('script');
-  script.src = 'https://graph.facebook.com/v2.10/1737081642972696?fields=name,fan_count&access_token=EAACEdEose0cBADB3NhwyAbEyODWj9bqGeh045kjYLy4MCANQ0gW5i5dCZAC3xUG9nV5LqxXCN7rtcTxXXkUStq0sOgNnHhZCfpmvVbejEY8ppiZAbt1JjNeIHqnZB9x0ZAWIxcXtIPpmTJHwRuKf9WyciagHejW72ZAyuOyi1m7vEaZCxC76iqSW1ZAhU7wrWt4ZD&callback=callbackLikesFB';
+
+  var acess_token = "EAACEdEose0cBAOwW62AwvDzZBNakrLYqXUXtvxaro9w1WubqBogVUkJkaZAw9ZBwND0hyVAci8se8hmaUNdZApe52eHb16efQBxYjynGymGi4pA5J6PWfqZC3jlI5WsJDQUCcrfx7SndVdkvjxZCoinAWE4uG5vzu39SDdwzG5M3VwZAXgZCaRFRBvvV04fbjSFUXLzK9ew9kwZDZD"
+
+  script.src = "https://graph.facebook.com/v2.10/1737081642972696?fields=name,fan_count&access_token="+acess_token+"&callback=callbackLikesFB";
   document.body.appendChild(script);
 });
