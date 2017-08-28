1762b705cb650b56eb55035d3c82d53aa5ec2161
dnovaes <novaesdigo@gmail.com>
2017-08-27 22:37:11 -0300

    adicionado função de scroll de volta ao sistema e função de pegar info de likes do GRAPH API EXPLORER do facebook

--- projeto/web/js/home.js
+++ projeto/web/js/home.js
@@ -1,17 +1,22 @@
+window.onload = function () {
+    initialize();
+    pegaTodosFornecedLatlg();
+};
+
+
 function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
 }
 
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
         var endereco_completo = conteudo.logradouro + ", " + conteudo.bairro + ", " + conteudo.localidade + " - " + conteudo.uf;
-        
-        var enderecocepDiv = document.getElementById('endereco_cep');
         carregarNoMapa(endereco_completo);
+        pegaTodosFornecedLatlg();
     } else {
         alert("CEP inválido.");
     }
@@ -30,22 +35,26 @@ function buscarcep(valor) { //gambiarra
             alert("Formato de CEP inválido.");
         }
     }
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
-function callbackLikesFB(content){
-  var likesfb = document.getElementById("likesFb");
-  likesfb.value = content.fan_count;
-  likesfb.innerHTML = content.fan_count;
-  console.log(content.fan_count);
-  likesfb.style.display="none";
+function callbackLikesFB(content) {
+    var divLikes = document.createElement("div");
+    divLikes.setAttribute("id", "likesFb");
+
+    document.body.appendChild(divLikes);
+
+    var likesfb = document.getElementById("likesFb");
+
+    likesfb.value = content.fan_count;
+    likesfb.innerHTML = content.fan_count;
+    likesfb.style.display = "none";
 }
 
 
-document.addEventListener("DOMContentLoaded", function(){
-  var script = document.createElement('script');
-
-  var acess_token = "EAACEdEose0cBAOwW62AwvDzZBNakrLYqXUXtvxaro9w1WubqBogVUkJkaZAw9ZBwND0hyVAci8se8hmaUNdZApe52eHb16efQBxYjynGymGi4pA5J6PWfqZC3jlI5WsJDQUCcrfx7SndVdkvjxZCoinAWE4uG5vzu39SDdwzG5M3VwZAXgZCaRFRBvvV04fbjSFUXLzK9ew9kwZDZD"
-
-  script.src = "https://graph.facebook.com/v2.10/1737081642972696?fields=name,fan_count&access_token="+acess_token+"&callback=callbackLikesFB";
-  document.body.appendChild(script);
+document.addEventListener("DOMContentLoaded", function () {
+    var script = document.createElement('script');
+    var acess_token = "EAACEdEose0cBACuNb9XpZCwe4bCdckk5grpncDL6ZAzBKTov4FW2PZB1CZBD4RWZCXZByRsAZCywkCBOb4Gagon5r86VWTlAK19ZBjVwSRrZBny2ZBZAmMZBtrpHxtHLvj2fFmSLegexCM8ZAMU8qIiUbmwKI5c1nIDjybINlARk0CoBlosFRZAvuc8aWB5tyujMuAi8QM1HyPbcs8cAZDZD";
+    script.src = "https://graph.facebook.com/v2.10/1737081642972696?fields=name,fan_count&access_token=" + acess_token + "&callback=callbackLikesFB";
+    document.body.appendChild(script);
 });
